ca95e743d97ce9ed546dc4837aa6a9e803922fde
wojciechtobis <wotektobis@gmail.com>
2016-10-20 08:23:52 -0700

    add states operation

--- simulation.h
+++ simulation.h
@@ -5,8 +5,8 @@
 #include<math.h>
 #include<stdlib.h>
 
-#define N 100
-#define T 10
+#define N 200
+#define T 20
 #define h0 0.01
 
 typedef struct State
@@ -19,5 +19,6 @@ void simulation();
 void RK4(State x[], float u[], float* t, float dtau[], int n[], int cn[], State x_zad, float P);
 State rhs(State x, float u);
 State add_states(State s1, State s2);
+State multiply_states(State s1, float s2);
 
 #endif
