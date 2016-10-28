6f1663bc9c6fd3d84cf22a9eb6f84df9b5bfc731
wojciechtobis <wotektobis@gmail.com>
2016-10-28 05:27:37 -0700

    Add printPopulation function

--- report.h
+++ report.h
@@ -4,9 +4,11 @@
 #include"simulation.h"
 #include <time.h>
 #include <string.h>
+#include "optim_problem.h"
 
 void getDate(char* date);
 void getStatesFileName(char* filename);
 void printStates(const State* x,int x_size,float* t);
+long long printPopulation(const long long population[],const float fitness_val[],int population_size, int population_number,long long global_best);
 
 #endif
